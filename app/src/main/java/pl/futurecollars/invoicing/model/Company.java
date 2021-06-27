5ffce76718edc5c873c5301f03790bd61e6b81a9
Samuel Janik <samjanik@gmail.com>
2021-06-27 15:08:24 +0200

    Added class structure and tests

---
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -0,0 +1,20 @@
+package pl.futurecollars.invoicing.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Company {
+
+    private int id;
+    private String taxIdentificationNumber;
+    private String address;
+    private String name;
+
+    public Company(String taxIdentificationNumber, String address, String name) {
+        this.taxIdentificationNumber = taxIdentificationNumber;
+        this.address = address;
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
@@ -0,0 +1,25 @@
+package pl.futurecollars.invoicing.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+public class Invoice {
+
+    private int id;
+    private LocalDate issueDate;
+    private Company buyer;
+    private Company seller;
+    private List<InvoiceEntry> entries;
+
+    public Invoice(LocalDate issueDate, Company buyer, Company seller, List<InvoiceEntry> entries) {
+        this.issueDate = issueDate;
+        this.buyer = buyer;
+        this.seller = seller;
+        this.entries = entries;
+    }
+}
