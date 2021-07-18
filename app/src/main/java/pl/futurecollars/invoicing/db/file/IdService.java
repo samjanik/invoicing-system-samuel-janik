09aa4b2bb52fc1853ef0fc92dd22a2c9f84104d9
Samuel Janik <samjanik@gmail.com>
2021-07-18 18:30:46 +0200

    Added missing files

---
+++ app/src/main/java/pl/futurecollars/invoicing/db/file/IdService.java
@@ -0,0 +1,44 @@
+package pl.futurecollars.invoicing.db.file;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import pl.futurecollars.invoicing.utils.FilesService;
+
+public class IdService {
+
+    private final Path idFilePath;
+    private final FilesService filesService;
+
+    private int nextId;
+
+    public IdService(Path idFilePath, FilesService filesService) {
+        this.idFilePath = idFilePath;
+        this.filesService = filesService;
+
+        try {
+            List<String> lastUsedID = filesService.readAllLines(idFilePath);
+            if (lastUsedID.isEmpty()) {
+                filesService.writeToFile(idFilePath, "1");
+                nextId = 1;
+            } else {
+                nextId = Integer.parseInt(lastUsedID.get(0));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to initialize id database", e);
+        }
+    }
+
+    public void printIDtoTracker() {
+        try {
+            filesService.writeToFile(idFilePath, String.valueOf(nextId + 1));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write to id file", e);
+        }
+    }
+
+    public int getNextIdAndIncrement() {
+        printIDtoTracker();
+        return nextId++;
+    }
+}
