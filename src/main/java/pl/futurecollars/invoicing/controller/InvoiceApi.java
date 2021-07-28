9ea9db7ef9c2e6d6e2054e6d1f2a18d5326e9199
Samuel Janik <samjanik@gmail.com>
2021-07-28 09:21:37 +0200

    Task #8 Documented endpoints with realistic data

---
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceApi.java
@@ -0,0 +1,37 @@
+package pl.futurecollars.invoicing.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.futurecollars.invoicing.model.Invoice;
+
+@Api(tags = {"invoice-controller"})
+public interface InvoiceApi {
+
+    @GetMapping(produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "Get list of all invoices")
+    List<Invoice> getAll();
+
+    @PostMapping(produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "Add new invoice to the system")
+    int save(@RequestBody Invoice invoice);
+
+    @GetMapping(value = "/{id}", produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "Get invoice by id")
+    ResponseEntity<Invoice> getById(@PathVariable int id);
+
+    @PutMapping(value = "/{id}", produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "Update invoice with given id")
+    ResponseEntity<Invoice> updateById(@PathVariable int id, @RequestBody Invoice invoice);
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete invoice with given id")
+    ResponseEntity<Invoice> deleteById(@PathVariable int id);
+}
--- src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,8 +9,14 @@ import lombok.NoArgsConstructor;
 public class Company {
 
     private int id;
+
+    @ApiModelProperty(value = "Tax identification number", required = true, example = "884-158-84-22")
     private String taxIdentificationNumber;
+
+    @ApiModelProperty(value = "Company address", required = true, example = "ul. Janickiego 123, 41-700 Ruda Śląska")
     private String address;
+
+    @ApiModelProperty(value = "Company name", required = true, example = "Software Development LLC.")
     private String name;
 
     public Company(String taxIdentificationNumber, String address, String name) {
