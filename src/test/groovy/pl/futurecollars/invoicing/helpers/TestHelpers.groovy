640f32cfd03929068b605cdc38c8d824e6787c1f
Samuel Janik <samjanik@gmail.com>
2021-07-29 10:51:02 +0200

    Task # 9 Implemented Builder Pattern

--- src/test/groovy/pl/futurecollars/invoicing/helpers/TestHelpers.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/helpers/TestHelpers.groovy
@@ -10,16 +10,28 @@ import java.time.LocalDate
 class TestHelpers {
 
     static company(int id) {
-        new Company(("$id").repeat(10),
-                "ul. Bukowińska 24d/$id 02-703 Warszawa, Polska",
-                "iCode Trust $id Sp. z o.o");
+        Company.builder()
+                .taxIdentificationNumber(("$id").repeat(10))
+                .address("ul. Bukowińska 24d/$id 02-703 Warszawa, Polska")
+                .name("iCode Trust $id Sp. z o.o")
+                .build()
     }
 
     static product(int id) {
-        new InvoiceEntry("Programming course $id", BigDecimal.valueOf(id * 1000), BigDecimal.valueOf(id * 1000 * 0.08), Vat.VAT_8)
+        InvoiceEntry.builder()
+                .description("Programming course $id")
+                .price(BigDecimal.valueOf(id * 1000))
+                .vatValue(BigDecimal.valueOf(id * 1000 * 0.08))
+                .vatRate(Vat.VAT_8)
+                .build()
     }
 
     static invoice(int id) {
-        new Invoice(LocalDate.now(), company(id), company(id), List.of(product(id)))
+        Invoice.builder()
+                .issueDate(LocalDate.now())
+                .buyer(company(id))
+                .seller(company(id + 1))
+                .entries(List.of(product(id)))
+                .build()
     }
 }
