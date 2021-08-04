233e668a3d0e1fd418c3950c8d41e2fad63d6a14
Samuel Janik <samjanik@gmail.com>
2021-08-04 12:19:36 +0200

    Task # 11 Added H2 Database dependency

---
+++ src/main/resources/db/migration/V1__create_invoice_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.invoice
+(
+    id          bigserial              NOT NULL,
+    issue_date  date                   NOT NULL,
+    "number"    character varying(50)  NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT  unique_invoice_number  UNIQUE ("number")
+);
