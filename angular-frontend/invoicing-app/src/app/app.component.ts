64edeacd884c9d75eb0bf54963cb0816c27d1cdd
Samuel Janik <samjanik@gmail.com>
2021-08-30 22:43:18 +0200

    Task 17 Created a fully functional application

--- angular-frontend/invoicing-app/src/app/app.component.ts
+++ angular-frontend/invoicing-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Company } from './model/company';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'invoicing-app';
+
+  title = 'Invoicing Application';
+  
+  companies: Company[] = [
+    new Company(
+        "123-3",
+        "krakow",
+        "Google",
+        123,
+        344
+    ),
+    new Company(
+        "444-3",
+        "Warsaw",
+        "Gogs",
+        333,
+        444
+    )
+  ];
+
+  newCompany: Company = new Company("", "", "", 0, 0);
+
+  addCompany() {
+      this.companies.push(this.newCompany);
+      this.newCompany = new Company("", "", "", 0, 0);
+  }
+
+  deleteCompany(companyToDelete: Company) {
+      this.companies = this.companies.filter(company => company !== companyToDelete);
+  }
+
+  triggerUpdate(company: Company) {
+      company.editedCompany = new Company(
+          company.taxIdentificationNumber,
+          company.address,
+          company.name,
+          company.healthInsurance,
+          company.pensionInsurance
+      )
+      company.editMode = true
+  }
+
+  cancelCompanyUpdate(company: Company) {
+      company.editMode = false;
+  }
+
+  updateCompany(updatedCompany: Company) {
+      updatedCompany.taxIdentificationNumber = updatedCompany.editedCompany.taxIdentificationNumber
+      updatedCompany.address = updatedCompany.editedCompany.address
+      updatedCompany.name = updatedCompany.editedCompany.name
+      updatedCompany.healthInsurance = updatedCompany.editedCompany.healthInsurance
+      updatedCompany.pensionInsurance = updatedCompany.editedCompany.pensionInsurance
+
+      updatedCompany.editMode = false;
+  }
+
 }
