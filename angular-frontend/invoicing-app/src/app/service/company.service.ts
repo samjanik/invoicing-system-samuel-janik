6899f0ac2d5c13c71768ec29e640b0bdab6cf193
Samuel Janik <samjanik@gmail.com>
2021-08-31 09:12:30 +0200

    Task 17 Developed a fully functional application with frontend in Angular and backed in Spring

---
+++ angular-frontend/invoicing-app/src/app/service/company.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Company } from "../model/company";
+
+const PATH = 'companies';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class CompanyService {
+
+    private contentType = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    constructor(private http: HttpClient) {
+    }
+
+    getCompanies(): Observable<Company[]> {
+        return this.http.get<Company[]>(this.apiUrl(PATH));
+    }
+
+    addCompany(company: Company): Observable<any> {
+        return this.http.post<any>(this.apiUrl(PATH), this.toCompanyRequest(company), this.contentType);
+    }
+
+    deleteCompany(id: number): Observable<any> {
+        return this.http.delete<any>(this.apiUrl(PATH, id));
+    }
+
+    editCompany(company: Company): Observable<any> {
+        return this.http.put<Company>(this.apiUrl(PATH, company.id), this.toCompanyRequest(company), this.contentType);
+    }
+
+    private apiUrl(service: string, id: number = null): string {
+        const idInUrl = (id !== null ? '/' + id : '');
+
+        return environment.apiUrl + '/' + service + idInUrl;
+    }
+
+    private toCompanyRequest(company: Company) {
+        return {
+            taxIdentificationNumber: company.taxIdentificationNumber,
+            name: company.name,
+            address: company.address,
+            healthInsurance: company.healthInsurance,
+            pensionInsurance: company.pensionInsurance,
+        };
+    }
+
+}
