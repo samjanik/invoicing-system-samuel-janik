f551edd0d0cf048f98e50661d3946bd34ee05090
Samuel Janik <samjanik@gmail.com>
2021-09-02 14:15:22 +0200

    Task # 18 Added E2E tests for companies components - displaying and adding, updated environment

--- angular-frontend/invoicing-app/e2e/src/company.e2e-spec.ts
+++ angular-frontend/invoicing-app/e2e/src/company.e2e-spec.ts
@@ -1,17 +1,37 @@
 import { CompanyPage } from './company.po';
-import { browser, logging } from 'protractor';
+import { browser, ExpectedConditions, logging } from 'protractor';
 
 describe('Company page E2E test', () => {
   let page: CompanyPage;
 
-  beforeEach(() => {
+  beforeEach(async() => {
     page = new CompanyPage();
+
+    await page.navigateTo();
+
+    await page.companyRows().each(async (row, index) => {
+      await page.deleteButton(row).click()
+    })
+
+    browser.wait(ExpectedConditions.not(ExpectedConditions.presenceOf(page.anyCompanyRow())))
+
+    expect(await page.companyRows()).toEqual([])
   });
 
-  it('should display welcome message', async () => {
+  it('should display correct values for table headers', async () => {
     await page.navigateTo();
     expect(await page.taxIdHeaderValue()).toEqual('Tax Identification Number');
+    expect(await page.nameHeaderValue()).toEqual('Name');
+    expect(await page.addressHeaderValue()).toEqual('Address');
+    expect(await page.pensionInsuranceHeaderValue()).toEqual('Pension Insurance');
+    expect(await page.healthInsuranceHeaderValue()).toEqual('Health Insurance');
   });
+
+  it('can add company', async () => {
+    await page.addNewCompany("123", "123 Inc.", "123 Wall Street", 1234, 123)
+    page.companyRows().then(rows => expect(rows.length).toEqual(1))
+  })
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- angular-frontend/invoicing-app/e2e/src/company.po.ts
+++ angular-frontend/invoicing-app/e2e/src/company.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementArrayFinder, ElementFinder, WebElement } from 'protractor';
 
 export class CompanyPage {
   async navigateTo(): Promise<unknown> {
@@ -6,6 +6,45 @@ export class CompanyPage {
   }
 
   async taxIdHeaderValue(): Promise<string> {
-    return element(by.id('#taxIdHeader')).getText();
+    return element(by.id('taxIdHeader')).getText();
   }
+
+  async nameHeaderValue(): Promise<string> {
+    return element(by.id('nameHeader')).getText();
+  }
+
+  async addressHeaderValue(): Promise<string> {
+      return element(by.id('addressHeader')).getText();
+  }
+
+  async pensionInsuranceHeaderValue(): Promise<string> {
+      return element(by.id('pensionInsuranceHeader')).getText();
+  }
+
+  async healthInsuranceHeaderValue(): Promise<string> {
+      return element(by.id('healthInsuranceHeader')).getText();
+  }
+
+  companyRows(): ElementArrayFinder {
+    return element.all(by.css('.companyRow'))
+}
+
+  anyCompanyRow(): ElementFinder {
+    return element(by.css('.companyRow'))
+}
+
+  deleteButton(row: ElementFinder): WebElement {
+    return row.element(by.css('.btn-danger'))
+}
+
+  async addNewCompany(taxId: string, name: string, address: string, pensionInsurance: number, healthInsurance: number) {
+    await element(by.css('input[name=taxIdentificationNumber]')).sendKeys(taxId)
+    await element(by.css('input[name=name]')).sendKeys(name)
+    await element(by.css('input[name=address]')).sendKeys(address)
+    await element(by.css('input[name=pensionInsurance]')).sendKeys(pensionInsurance)
+    await element(by.css('input[name=healthInsurance]')).sendKeys(healthInsurance)
+    await element(by.id("createCompanyButton")).click
+  }
+
+
 }
